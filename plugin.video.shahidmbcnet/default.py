11e0c0157ac2dc406ee86ac0c91694132519b9b9
Shani-08 <ShahjahanDurrani@hotmail.com>
2014-03-03 00:12:22 +0000

    mbc
    mbc

---
+++ plugin.video.shahidmbcnet/default.py
@@ -0,0 +1,362 @@
+# -*- coding: utf-8 -*-
+import xbmc, xbmcgui, xbmcplugin
+import urllib2,urllib,cgi, re, urlresolver
+import urlparse
+import HTMLParser
+import xbmcaddon
+from operator import itemgetter
+import json
+
+__addon__       = xbmcaddon.Addon()
+__addonname__   = __addon__.getAddonInfo('name')
+__icon__        = __addon__.getAddonInfo('icon')
+addon_id = 'plugin.video.shahidmbcnet'
+selfAddon = xbmcaddon.Addon(id=addon_id)
+  
+ 
+mainurl='http://shahid.mbc.net'
+liveURL='http://www.zemtv.com/live-pakistani-news-channels/'
+
+tabURL ='http://www.eboundservices.com:8888/users/rex/m_live.php?app=%s&stream=%s'
+VIEW_MODES = {
+    'thumbnail': {
+        'skin.confluence': 500,
+        'skin.aeon.nox': 551,
+        'skin.confluence-vertical': 500,
+        'skin.jx720': 52,
+        'skin.pm3-hd': 53,
+        'skin.rapier': 50,
+        'skin.simplicity': 500,
+        'skin.slik': 53,
+        'skin.touched': 500,
+        'skin.transparency': 53,
+        'skin.xeebo': 55,
+    },
+}
+
+def get_view_mode_id( view_mode):
+	view_mode_ids = VIEW_MODES.get(view_mode.lower())
+	if view_mode_ids:
+		return view_mode_ids.get(xbmc.getSkinDir())
+	return None
+
+def addLink(name,url,iconimage):
+	ok=True
+	liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+	liz.setInfo( type="Video", infoLabels={ "Title": name } )
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+	return ok
+
+def Colored(text = '', colorid = '', isBold = False):
+	if colorid == 'ZM':
+		color = 'FF11b500'
+	elif colorid == 'EB':
+		color = 'FFe37101'
+	elif colorid == 'bold':
+		return '[B]' + text + '[/B]'
+	else:
+		color = colorid
+		
+	if isBold == True:
+		text = '[B]' + text + '[/B]'
+	return '[COLOR ' + color + ']' + text + '[/COLOR]'	
+	
+def addDir(name,url,mode,iconimage	,showContext=False, showLiveContext=False,isItFolder=True,pageNumber=""):
+#	print name
+#	name=name.decode('utf-8','replace')
+	h = HTMLParser.HTMLParser()
+	name= h.unescape(name).decode("utf-8")
+	#print ' printing name'
+	rname=  name.encode("utf-8")
+	#print rname
+	#print iconimage
+	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(rname)
+	if len(pageNumber):
+		u+="&pagenum="+pageNumber
+	ok=True
+#	print iconimage
+	liz=xbmcgui.ListItem(rname, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+	#liz.setInfo( type="Video", infoLabels={ "Title": name } )
+
+	if showContext==True:
+		cmd1 = "XBMC.RunPlugin(%s&linkType=%s)" % (u, "DM")
+		cmd2 = "XBMC.RunPlugin(%s&linkType=%s)" % (u, "LINK")
+		cmd3 = "XBMC.RunPlugin(%s&linkType=%s)" % (u, "Youtube")
+		liz.addContextMenuItems([('Play Youtube video',cmd3),('Play DailyMotion video',cmd1),('Play Tune.pk video',cmd2)])
+	
+	if showLiveContext==True:
+		cmd1 = "XBMC.RunPlugin(%s&linkType=%s)" % (u, "RTMP")
+		cmd2 = "XBMC.RunPlugin(%s&linkType=%s)" % (u, "HTTP")
+		liz.addContextMenuItems([('Play RTMP Steam (flash)',cmd1),('Play Http Stream (ios)',cmd2)])
+	
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=isItFolder)
+	return ok
+	
+
+
+def get_params():
+	param=[]
+	paramstring=sys.argv[2]
+	if len(paramstring)>=2:
+		params=sys.argv[2]
+		cleanedparams=params.replace('?','')
+		if (params[len(params)-1]=='/'):
+			params=params[0:len(params)-2]
+		pairsofparams=cleanedparams.split('&')
+		param={}
+		for i in range(len(pairsofparams)):
+			splitparams={}
+			splitparams=pairsofparams[i].split('=')
+			if (len(splitparams))==2:
+				param[splitparams[0]]=splitparams[1]
+				
+	return param
+
+
+
+
+def Addtypes():
+	#2 is series=3 are links
+	addDir('Channels' ,'http://shahid.mbc.net/ar/channel-browser.html' ,2,'') #links #2 channels,3 series,4 video entry, 5 play
+	addDir('All Series' ,'http://shahid.mbc.net/ar/series-browser.html' ,6,'')
+	#addDir('GeoTv Shows' ,'http://www.dramasonline.com/geo-tv-latest-dramas-episodes-online/' ,2,'')
+	#addDir('AryDigital Shows' ,'http://www.dramasonline.com/ary-digital-tv-latest-dramas-episodes-online/' ,2,'')
+	#addDir('Hum Sitaray Shows' ,'http://www.dramasonline.com/hum-sitaray-latest-dramas-episodes-online/' ,2,'')
+	#addDir('Express Shows' ,'http://www.dramasonline.com/express-entertainment-latest-dramas-episodes-online/' ,2,'')
+	#addDir('APlus Shows' ,'http://www.dramasonline.com/aplus-entertainment-latest-dramas-episodes-online/' ,2,'')
+	#addDir('Teleplays' ,'http://www.dramasonline.com/?cat=255' ,3,'')# these are is links
+	#addDir('Top Rated Dramas' ,'http://www.dramasonline.com/' ,5,'') # top 
+	#addDir('Live Channels' ,'http://www.dramasonline.com/category/live-channels/' ,6,'') ##
+	#addDir('Settings' ,'http://www.dramasonline.com/category/live-channels/' ,8,'',isItFolder=False) ##
+	return
+
+def ShowSettings(Fromurl):
+	selfAddon.openSettings()
+
+def AddSeries(Fromurl,pageNumber=""):
+#	print Fromurl
+	req = urllib2.Request(Fromurl)
+	req.add_header('User-Agent','Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+	print Fromurl
+#	print "addshows"
+#	match=re.compile('<param name="URL" value="(.+?)">').findall(link)
+#	match=re.compile('<a href="(.+?)"').findall(link)
+#	match=re.compile('onclick="playChannel\(\'(.*?)\'\);">(.*?)</a>').findall(link)
+#	match =re.findall('onclick="playChannel\(\'(.*?)\'\);">(.*?)</a>', link, re.DOTALL|re.IGNORECASE)
+#	match =re.findall('onclick="playChannel\(\'(.*?)\'\);".?>(.*?)</a>', link, re.DOTALL|re.IGNORECASE)
+#	match =re.findall('<div class=\"post-title\"><a href=\"(.*?)\".*<b>(.*)<\/b><\/a>', link, re.IGNORECASE)
+#	match =re.findall('<img src="(.*?)" alt=".*".+<\/a>\n*.+<div class="post-title"><a href="(.*?)".*<b>(.*)<\/b>', link, re.UNICODE)
+	#regstring='<a href="(.*?)">[\s\t\r\n\f]*.*[\s\t\r\n\f]+.*[\s\t\r\n\f].*<img .*?alt="(.*?)" src="(.*?)"'
+	regstring='<a href="(\/ar\/(show|series).*?)">\s.*\s*.*\s.*alt="(.*)" src="(.*?)" '
+	match =re.findall(regstring, link)
+	#print match
+	#match=re.compile('<a href="(.*?)"targe.*?<img.*?alt="(.*?)" src="(.*?)"').findall(link)
+	#print Fromurl
+
+
+	for cname in match:
+		addDir(cname[2] ,mainurl+cname[0] ,4,cname[3])#name,url,img
+	if mode==6:
+		if not pageNumber=="":
+			pageNumber=str(int(pageNumber)+1);#parseInt(1)+1;
+		else:
+			pageNumber="1";
+
+		purl='http://shahid.mbc.net/ar/series-browser/autoGeneratedContent/seriesBrowserGrid~browse~-param-.sort-latest.pageNumber-%s.html' % pageNumber
+		addDir('Next Page' ,purl ,6,'', False, True,pageNumber=pageNumber)		#name,url,mode,icon
+	
+#	<a href="http://www.zemtv.com/page/2/">&gt;</a></li>
+#	match =re.findall('<a href="(.*)">&gt;<\/a><\/li>', link, re.IGNORECASE)
+	
+#	if len(match)==1:
+#		addDir('Next Page' ,match[0] ,2,'')
+#       print match
+	
+	return
+
+def AddEnteries(Fromurl):
+#	print Fromurl
+	req = urllib2.Request(Fromurl)
+	req.add_header('User-Agent','Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+#	print link
+#	print "addshows"
+#	match=re.compile('<param name="URL" value="(.+?)">').findall(link)
+#	match=re.compile('<a href="(.+?)"').findall(link)
+#	match=re.compile('onclick="playChannel\(\'(.*?)\'\);">(.*?)</a>').findall(link)
+#	match =re.findall('onclick="playChannel\(\'(.*?)\'\);">(.*?)</a>', link, re.DOTALL|re.IGNORECASE)
+#	match =re.findall('onclick="playChannel\(\'(.*?)\'\);".?>(.*?)</a>', link, re.DOTALL|re.IGNORECASE)
+#	match =re.findall('<div class=\"post-title\"><a href=\"(.*?)\".*<b>(.*)<\/b><\/a>', link, re.IGNORECASE)
+#	match =re.findall('<img src="(.*?)" alt=".*".+<\/a>\n*.+<div class="post-title"><a href="(.*?)".*<b>(.*)<\/b>', link, re.UNICODE)
+#	print Fromurl
+	match =re.findall('<a href="(\/ar\/episode.*?)">\s.*\s.*\s.*\s.*\s.*\s.*<img .*?alt="(.*?)" src="(.*?)".*\s.*.*\s.*.*\s.*.*\s.*.*\s.*.*\s.*\s*.*?>(.*?)<\/div>\s*.*?>(.*?)<\/div>', link, re.UNICODE)
+#	print Fromurl
+
+	#print match
+	#h = HTMLParser.HTMLParser()
+	
+	#print match
+	for cname in match:
+		finalName=cname[1]
+		#print 'a1'
+		
+		#if len(finalName)>0: finalName+=u" ";
+		#print 'a2'
+		
+		finalName+=cname[4]
+		#print 'a3'
+		#finalName+=cname[3]
+		#print 'a4'
+		
+		addDir(finalName ,mainurl+cname[0] ,5,cname[2],isItFolder=False)
+		
+#	<a href="http://www.zemtv.com/page/2/">&gt;</a></li>
+	#match =re.findall('<link rel=\'next\' href=\'(.*?)\' \/>', link, re.IGNORECASE)
+	
+	#if len(match)==1:
+#		addDir('Next Page' ,match[0] ,3,'')
+#       print match
+	
+	return
+	
+def AddChannels(liveURL):
+	req = urllib2.Request(liveURL)
+	req.add_header('User-Agent','Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+#	print link
+#	match=re.compile('<param name="URL" value="(.+?)">').findall(link)
+#	match=re.compile('<a href="(.+?)"').findall(link)
+#	match=re.compile('onclick="playChannel\(\'(.*?)\'\);">(.*?)</a>').findall(link)
+#	match =re.findall('onclick="playChannel\(\'(.*?)\'\);">(.*?)</a>', link, re.DOTALL|re.IGNORECASE)
+#	match =re.findall('onclick="playChannel\(\'(.*?)\'\);".?>(.*?)</a>', link, re.DOTALL|re.IGNORECASE)
+#	match =re.findall('<div class=\"post-title\"><a href=\"(.*?)\".*<b>(.*)<\/b><\/a>', link, re.IGNORECASE)
+#	match =re.findall('<img src="(.*?)" alt=".*".+<\/a>\n*.+<div class="post-title"><a href="(.*?)".*<b>(.*)<\/b>', link, re.UNICODE)
+
+	match =re.findall('<div class="subitem".*?id="(.*)">\s*.*\s*<a href="(.*?)".*\s.*\s*<.*src="(.*?)"', link,re.M)
+
+	#print match
+	#h = HTMLParser.HTMLParser()
+	for cname in match:
+		addDir(cname[0] ,mainurl+cname[1] ,3,cname[2], False, True,isItFolder=True)		#name,url,mode,icon
+
+	return	
+	
+
+def PlayShowLink ( url ): 
+#	url = tabURL.replace('%s',channelName);
+	req = urllib2.Request(url)
+	req.add_header('User-Agent', 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+#	print url
+
+	line1 = "Playing video"
+	time = 5000  #in miliseconds
+	line1 = "Playing video Link"
+	xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time, __icon__))
+
+	#print "PlayLINK"
+	playURL= match =re.findall('id  : "(.*?)",\s*pricingPlanId  : "(.*?)"', link)
+	videoID=match[0][0]# check if not found then try other methods
+	paymentID=match[0][1]
+	playlistURL="http://shahid.mbc.net/arContent/getPlayerContent-param-.id-%s.type-player.pricingPlanId-%s.html" % ( videoID,paymentID)
+	req = urllib2.Request(playlistURL)
+	req.add_header('User-Agent', 'Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10')
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+	jsonData=json.loads(link)
+	#print jsonData;
+	url=jsonData["data"]["url"]
+	#print url
+	cName='test'
+	listitem = xbmcgui.ListItem( label = str(cName), iconImage = "DefaultVideo.png", thumbnailImage = xbmc.getInfoImage( "ListItem.Thumb" ), path=url )
+	#print "playing stream name: " + str(cName) 
+	listitem.setInfo( type="video", infoLabels={ "Title": cName, "Path" : url } )
+	listitem.setInfo( type="video", infoLabels={ "Title": cName, "Plot" : cName, "TVShowTitle": cName } )
+	print 'playurl',url
+	xbmc.Player().play( url,listitem)
+	#print 'ol..'
+	return
+	
+
+
+
+	
+#print "i am here"
+params=get_params()
+url=None
+name=None
+mode=None
+linkType=None
+pageNumber=None
+
+try:
+	url=urllib.unquote_plus(params["url"])
+except:
+	pass
+try:
+	name=urllib.unquote_plus(params["name"])
+except:
+	pass
+try:
+	mode=int(params["mode"])
+except:
+	pass
+
+try:
+	pageNumber=params["pagenum"]
+except:
+	pageNumber="";
+
+args = cgi.parse_qs(sys.argv[2][1:])
+linkType=''
+try:
+	linkType=args.get('linkType', '')[0]
+except:
+	pass
+
+
+print 	mode,pageNumber
+
+try:
+	if mode==None or url==None or len(url)<1:
+		print "InAddTypes"
+		Addtypes()
+
+	elif mode==2:
+		print "Ent url is "+name,url
+		AddChannels(url)
+
+	elif mode==3 or mode==6:
+		print "Ent url is "+url
+		AddSeries(url,pageNumber)
+
+	elif mode==4:
+		print "Play url is "+url
+		AddEnteries(url)
+
+	elif mode==5:
+		PlayShowLink(url)
+	elif mode==8:
+		print "Play url is "+url,mode
+		ShowSettings(url)
+except:
+	print 'somethingwrong'
+
+if (not mode==None) and mode>1:
+	view_mode_id = get_view_mode_id('thumbnail')
+	if view_mode_id is not None:
+		print 'view_mode_id',view_mode_id
+		xbmc.executebuiltin('Container.SetViewMode(%d)' % view_mode_id)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
